2dc28ca3a56e4a9f14518fa3adcaca2d08cee8e3
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2023-10-13 09:15:49 +0530

    committed 10/13/2023, 9:15:48 AM

---
+++ set01/problem04.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void input(int *a, int *b){
+    printf("Enter two numbers:\n");
+    scanf("%d %d",a, b);
+}
+void add(int a, int b, int *sum){
+    *sum=0;
+    *sum=a+b;
+}
+void output(int a, int b, int sum){
+    printf("Sum of %d and %d is %d",a, b, sum);
+}
+int main(){
+    int a, b;
+    input(&a, &b);
+    int sum;
+    add(a,b,&sum);
+    output(a, b, sum);
+    return 0;
+}
