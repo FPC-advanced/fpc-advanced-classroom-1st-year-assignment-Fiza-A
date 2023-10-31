3e06af52ebb4b35aaeb1a88517e84c2e94f240f2
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2023-10-31 20:21:25 +0530

    committed 10/31/2023, 8:21:24 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -18,14 +18,14 @@ int input_number(){
 int is_prime(int n){
     if(n<=1){
         printf("Number should be greater than 1!");
-        return 0;
+        return 1;
     }
     for(int i=2; i<=n/2; i++){
         if(n%i==0){
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 void output(int n ,int result){
     if(result==-1){
