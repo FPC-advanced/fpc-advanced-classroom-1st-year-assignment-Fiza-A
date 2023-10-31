ad47341e1a7ba471dd320e99fedbd9aeb261ac5b
Fiza-A <fizaanwar.bsc23@rvu.edu.in>
2023-10-31 21:54:09 +0530

    committed 10/31/2023, 9:54:08 PM

---
+++ set02/problem06.c
@@ -0,0 +1,30 @@
+/*06. Write a program to reverse a string.
+
+***Function Declarations***
+```c
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+```*/
+#include<stdio.h>
+void input_string(char *a){
+    printf("Enter a string:\n");
+    scanf("%s", a);
+}
+void str_reverse(char *str, char *rev_str) {
+    int length = strlen(str);
+    for (int i = 0; i < length; i++) {
+        rev_str[i] = str[length - 1 - i];
+    }
+    rev_str[length] = '\0';
+}
+void output(char *a, char *reverse_a) {
+    printf("\nReversed String: %s\n", reverse_a);
+}
+int main() {
+    char a[50], reverse_a[50];
+    input_string(a);
+    str_reverse(a, reverse_a);
+    output(a, reverse_a);
+    return 0;
+}
